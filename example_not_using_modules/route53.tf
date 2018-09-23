de737a212d15476194e06d55a3695f4e9c791f11
Norihiro Sakamoto <nori0620@gmail.com>
2018-09-23 12:54:32 +0900

    Initial Commit

---
+++ example_not_using_modules/route53.tf
@@ -0,0 +1,23 @@
+# ---------------
+# route53.tf
+# ---------------
+
+resource "aws_route53_zone" "site_zone" {
+  name = "${var.root_domain}"
+
+  tags {
+    Name = "${var.root_domain}"
+  }
+}
+
+resource "aws_route53_record" "site" {
+  zone_id = "${aws_route53_zone.site_zone.zone_id}"
+  name = "${var.site_domain}"
+  type = "A"
+
+  alias {
+    name = "${aws_cloudfront_distribution.site.domain_name}"
+    zone_id = "${aws_cloudfront_distribution.site.hosted_zone_id}"
+    evaluate_target_health = false
+  }
+}
---
+++ example_not_using_modules/variables.tf
@@ -0,0 +1,15 @@
+variable "site_domain" {
+  default = "foo.example.com"
+}
+
+variable "root_domain" {
+  default = "example.com"
+}
+
+variable "bucket_name" {
+  default = "site-foo.example.com"
+}
+
+variable "not_found_file_path" {
+  default  = "/404.html"
+}
